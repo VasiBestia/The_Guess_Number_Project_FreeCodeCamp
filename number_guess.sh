eb0e905b387c6f4aa31d7015c5fc88c71ad78e3a
Vasilescu Alexandru Gabriel <162467637+VasiBestia@users.noreply.github.com>
2026-01-19 14:31:14 -0500

    chore:Adding first part of else the game

--- number_guess.sh
+++ number_guess.sh
@@ -16,13 +16,15 @@ echo Welcome, $USERNAME! It looks like this is your first time here.
 
 echo Guess the secret number between 1 and 1000:
 read SECRET_NUMBER
+CONTOR=1;
 
   while [[ "$SECRET_NUMBER" -ne "$THE_GUESS_NUMBER" ]]
 do
    if [[ ! $SECRET_NUMBER =~ ^[0-9]+$ ]]
    then
       echo "That is not an integer, guess again:"
-      read $SECRET_NUMBER
+      read SECRET_NUMBER
+      (( CONTOR++ ))
       continue
    fi
 
@@ -30,24 +32,53 @@ do
           then 
               echo "It's lower than that, guess again:"
               read SECRET_NUMBER
+              (( CONTOR++ ))
           elif [[ $THE_GUESS_NUMBER -gt $SECRET_NUMBER ]] 
               then
                   echo "It's higher than that, guess again:"
                   read SECRET_NUMBER
+                  (( CONTOR++ ))
   fi
 done
-echo You guessed it in number_of_guesses tries. The secret number was secret_number. Nice job!
+echo "You guessed it in $CONTOR tries. The secret number was $THE_GUESS_NUMBER. Nice job!"
+
+INSERT_INTO_DATABASE=$($PSQL "Insert Into guesser(username,games_played,best_game,number_of_tries) 
+                              Values('$USERNAME',1,$CONTOR,$CONTOR)");
+
 else
    GAMES_PLAYED=$($PSQL "Select games_played From guesser Where username='$USERNAME'");
    echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took <best_game> guesses.
 echo Guess the secret number between 1 and 1000:
 read SECRET_NUMBER
+CONTOR=1;
 
-echo It's lower than that, guess again:
-echo It's higher than that, guess again:
-echo That is not an integer, guess again:
-echo You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!
+  while [[ "$SECRET_NUMBER" -ne "$THE_GUESS_NUMBER" ]]
+do
+   if [[ ! $SECRET_NUMBER =~ ^[0-9]+$ ]]
+   then
+      echo "That is not an integer, guess again:"
+      read SECRET_NUMBER
+      (( CONTOR++ ))
+      continue
+   fi
 
+   if [[ $THE_GUESS_NUMBER -lt $SECRET_NUMBER ]] 
+          then 
+              echo "It's lower than that, guess again:"
+              read SECRET_NUMBER
+              (( CONTOR++ ))
+          elif [[ $THE_GUESS_NUMBER -gt $SECRET_NUMBER ]] 
+              then
+                  echo "It's higher than that, guess again:"
+                  read SECRET_NUMBER
+                  (( CONTOR++ ))
+  fi
+done
+  echo "You guessed it in $CONTOR tries. The secret number was $THE_GUESS_NUMBER. Nice job!"
+
+  ((GAMES_PLAYED++))
+  INSERT_INTO_DATABASE=$($PSQL "Insert Into guesser(username,games_played,best_game,number_of_tries) 
+                              Values('$USERNAME',$GAMES_PLAYED,$CONTOR,$CONTOR)");
 fi
 
 
