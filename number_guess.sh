37833009791a60ec51603aaacc8f28eb2025ba5f
Vasilescu Alexandru Gabriel <162467637+VasiBestia@users.noreply.github.com>
2026-01-19 14:02:10 -0500

    chore:Adding first condition

--- number_guess.sh
+++ number_guess.sh
@@ -7,7 +7,12 @@ THE_GUESS_NUMBER=$(( ( RANDOM % 1000 )  + 1 ))
 echo Enter your username:
 read USERNAME
 
-echo Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
+CHECKER_USERNAME=$($PSQL "Select 1 From guesser Where username='$USERNAME'")
+
+if [[ -z $CHECKER_USERNAME ]]
+then
+
+echo Welcome, $USERNAME! It looks like this is your first time here.
 echo Guess the secret number between 1 and 1000:
 read SECRET_NUMBER
 
@@ -16,7 +21,9 @@ echo It's higher than that, guess again:
 echo That is not an integer, guess again:
 echo You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!
 
-echo Welcome, <username>! It looks like this is your first time here.
+else
+   GAMES_PLAYED=$($PSQL "Select games_played From guesser Where username='$USERNAME'");
+   echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took <best_game> guesses.
 echo Guess the secret number between 1 and 1000:
 read SECRET_NUMBER
 
@@ -24,3 +31,9 @@ echo It's lower than that, guess again:
 echo It's higher than that, guess again:
 echo That is not an integer, guess again:
 echo You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!
+
+fi
+
+
+
+
